5393f0d9417879143194e3c4b149f47513a0b50a
FazWarr <warraichsoftware@gmail.com>
2025-03-16 13:18:17 -0700

    added base code

---
+++ apps.js
@@ -0,0 +1,17 @@
+(function() {
+    // let screen = document.querySelector('.screen');
+    // let buttons = document.querySelectorAll('.btn');
+    // let clear = document.querySelector('.btn-clear');
+    // let equal = document.querySelector('.btn-equal');
+
+    // buttons.forEach(function(button) {
+    //     button.addEventListener('keypress', function(e) {
+    //         let value = e.key;
+    //         console.log('Key pressed: ' + e.key);
+    //         if (value !== undefined) {
+    //             screen.value += value;
+    //         }
+    //     });
+    // });
+
+})()
