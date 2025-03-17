d835a255e898a930717744ef81b36dbca74532ea
FazWarr <warraichsoftware@gmail.com>
2025-03-16 18:34:34 -0700

    Guess the number mini-game is functional!

--- apps.js
+++ apps.js
@@ -1,17 +1,26 @@
-(function() {
-    // let screen = document.querySelector('.screen');
-    // let buttons = document.querySelectorAll('.btn');
-    // let clear = document.querySelector('.btn-clear');
-    // let equal = document.querySelector('.btn-equal');
 
-    // buttons.forEach(function(button) {
-    //     button.addEventListener('keypress', function(e) {
-    //         let value = e.key;
-    //         console.log('Key pressed: ' + e.key);
-    //         if (value !== undefined) {
-    //             screen.value += value;
-    //         }
-    //     });
-    // });
 
-})()
+function generateRandNum() {
+    min = Math.ceil(0);
+    max = Math.floor(9);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function display() {
+    document.write(generateRandNum());   
+}
+
+const button = document.querySelector("input");
+const paragraph = document.querySelector("p");
+
+button.addEventListener("click", updateButton);
+
+function updateButton() {
+  if (button.value === "Guess the number!") {
+    button.value = "Guess the number again!";
+    paragraph.textContent = generateRandNum();
+  } else {
+    button.value = "Guess the number!";
+    paragraph.textContent = generateRandNum();
+  }
+}
